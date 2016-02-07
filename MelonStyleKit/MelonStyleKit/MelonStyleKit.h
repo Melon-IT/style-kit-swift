713660720247939b7e6e816a43f8fa86a7ccc96a
Tomek Popis <tomek@melon-it.com>
2016-02-07 17:54:44 +0100

    Configure project

---
+++ MelonStyleKit/MelonStyleKit/MelonStyleKit.h
@@ -0,0 +1,19 @@
+//
+//  MelonStyleKit.h
+//  MelonStyleKit
+//
+//  Created by Tomasz Popis on 07/02/16.
+//  Copyright © 2016 Melon-IT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MelonStyleKit.
+FOUNDATION_EXPORT double MelonStyleKitVersionNumber;
+
+//! Project version string for MelonStyleKit.
+FOUNDATION_EXPORT const unsigned char MelonStyleKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MelonStyleKit/PublicHeader.h>
+
+
