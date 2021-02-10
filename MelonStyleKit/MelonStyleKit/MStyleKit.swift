de51cab3aa3b51e8b16cd76ea7731eb6112d8530
Tomek Popis <tomek@melon-it.com>
2021-02-10 13:37:48 +0100

    Add comments

--- MelonStyleKit/MelonStyleKit/MStyleKit.swift
+++ MelonStyleKit/MelonStyleKit/MStyleKit.swift
@@ -10,12 +10,13 @@ import Foundation
 
 typealias RGBA = (red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat)
 
-public protocol MStyleKitPlatformKeyDelegate: class {
+public protocol MStyleKitDeviceKeyDelegate: class {
   func styleIdentifier(for key: String) -> String
 }
 
 public class MStyleKit {
   
+  //MARK: Keys
   private static let colorsKey = "colors"
   private static let fontsKey = "fonts"
   private static let stylesKey = "styles"
@@ -27,30 +28,60 @@ public class MStyleKit {
   private static let paragraphSpacingKey = "paragraph-spacing"
   private static let lineSpacingKey = "line-spacing"
   
-  public weak var keyDecoderDelegate: MStyleKitPlatformKeyDelegate?
+  //MARK: Delegate
+  /**
+   Delegate that determine prefix for styles specific to the concrete device.
+   */
+  public weak var keyDelegate: MStyleKitDeviceKeyDelegate?
   
+  //MARK: Default style
   private var defaultColor: UIColor = UIColor.black
   //private var defaultStyle: Dictionary<String,AnyObject>?
   
+  //MARK: Collection
+  /**
+   Styles definitions read from .plist resources
+   */
   private var collection: Dictionary<String,AnyObject>?
   
+  //MARK: Initializing
+  /**
+   Creates the StyleKit object with resources from a bundle.
+   
+   - parameters:
+      - name: Name of the resource .plist file with style definitions.
+      - bundle: Name of the bundle where resource file is located.
+   */
   public init(resource name: String,
               from bundle:Bundle) {
     
     self.prepare(resource: name, from: bundle)
   }
-  
+  /**
+   Creates the StyleKit object with resources from the main bundle.
+   
+   - parameters:
+      - name: Name of the resource .plist file with style definitions.
+   */
   public convenience init(resource name: String) {
     self.init(resource: name, from: Bundle.main)
   }
   
+  //MARK: Reading attributes
+  /**
+   Make collection of attributes for the `NSAttributedString`.
+   
+   - parameters:
+      - key: Identifier for particular style in collection.
+      - alignment: Value that if it exist it override text alignment property. This argument is optional.
+   */
   public func attributes(for key: String,
-                         textAlignment: NSTextAlignment = .natural) -> [NSAttributedString.Key : Any] {
+                         alignment: NSTextAlignment = .natural) -> [NSAttributedString.Key : Any] {
     
     var textAttributes = Dictionary<NSAttributedString.Key,Any>()
     let paragraphAttributes = NSMutableParagraphStyle()
     
-    if var styleKey = self.keyDecoderDelegate?.styleIdentifier(for: key) {
+    if var styleKey = self.keyDelegate?.styleIdentifier(for: key) {
       if self.existAttributes(for: styleKey) == false {
         styleKey = key
       }
@@ -62,7 +93,7 @@ public class MStyleKit {
          let colorKey = style[MStyleKit.textColorKey] as? String,
          let fontSize = style[MStyleKit.textSizeKey] as? Float,
          let fontKey = style[MStyleKit.fontKey] as? String,
-         let alignment = style[MStyleKit.textAlignmentKey] as? Int,
+         let textAlignment = style[MStyleKit.textAlignmentKey] as? Int,
          let paragraphSpacing = style[MStyleKit.paragraphSpacingKey] as? Float,
          let lineSpacing = style[MStyleKit.lineSpacingKey] as? Float {
         
@@ -70,7 +101,7 @@ public class MStyleKit {
         font = self.font(for: fontKey, size: fontSize)
         
         paragraphAttributes.paragraphSpacing = CGFloat(paragraphSpacing);
-        paragraphAttributes.alignment = NSTextAlignment(rawValue: alignment)!;
+        paragraphAttributes.alignment = NSTextAlignment(rawValue: textAlignment)!;
         paragraphAttributes.lineBreakMode = NSLineBreakMode.byWordWrapping;
         paragraphAttributes.lineSpacing = CGFloat(lineSpacing);
         
@@ -78,8 +109,8 @@ public class MStyleKit {
         textAttributes[NSAttributedString.Key.font] = font
         textAttributes[NSAttributedString.Key.foregroundColor] = fontColor
         
-        if textAlignment != NSTextAlignment.natural {
-          paragraphAttributes.alignment = textAlignment
+        if alignment != NSTextAlignment.natural {
+          paragraphAttributes.alignment = alignment
         }
       }
     }
@@ -87,8 +118,9 @@ public class MStyleKit {
     return textAttributes
   }
   
+  //MARK: Reading file
   private func prepare(resource name: String?,
-                      from bundle: Bundle = Bundle.main) {
+                       from bundle: Bundle = Bundle.main) {
     
     if let path = bundle.path(forResource: name, ofType: "plist"),
        let data = try? Data(contentsOf: URL(fileURLWithPath: path)) {
@@ -100,14 +132,20 @@ public class MStyleKit {
     }
   }
   
+  //MARK: Retrieving information from resources
+  private func existAttributes(for key: String) -> Bool {
+    
+    return self.collection?[MStyleKit.stylesKey]?[key] is Dictionary<String, AnyObject>
+  }
+  
   private func color(for key: String,
-                    withAlpha: Float = 1.0) -> UIColor {
+                     withAlpha: Float = 1.0) -> UIColor {
     
     var result = self.defaultColor
     
     var color: RGBA
     
-    if var styleKey = self.keyDecoderDelegate?.styleIdentifier(for: key) {
+    if var styleKey = self.keyDelegate?.styleIdentifier(for: key) {
       
       if self.existAttributes(for: styleKey) == false {
         styleKey = key
@@ -123,6 +161,26 @@ public class MStyleKit {
                          green: color.green,
                          blue: color.blue,
                          alpha: color.alpha)
+      }
+    }
+    
+    return result
+  }
+  
+  private func font(for key: String,
+                    size: Float) -> UIFont {
+    
+    var result: UIFont = UIFont.systemFont(ofSize: CGFloat(size))
+    
+    if var styleKey = self.keyDelegate?.styleIdentifier(for: key) {
+      if self.existAttributes(for: styleKey) == false {
+        styleKey = key
+      }
+      
+      if let fontName = self.collection?[MStyleKit.fontsKey]?[styleKey] as? String,
+         let font =  UIFont(name: fontName, size: CGFloat(size)) {
+        
+        result = font
       }
     }
     
@@ -153,31 +211,6 @@ public class MStyleKit {
       
       if let alpha = Float(components[3]) {
         result.alpha = CGFloat(alpha)
-      }
-    }
-    
-    return result
-  }
-  
-  private func existAttributes(for key: String) -> Bool {
-    
-    return self.collection?[MStyleKit.stylesKey]?[key] is Dictionary<String, AnyObject>
-  }
-  
-  private func font(for key: String,
-                    size: Float) -> UIFont {
-    
-    var result: UIFont = UIFont.systemFont(ofSize: CGFloat(size))
-    
-    if var styleKey = self.keyDecoderDelegate?.styleIdentifier(for: key) {
-      if self.existAttributes(for: styleKey) == false {
-        styleKey = key
-      }
-      
-      if let fontName = self.collection?[MStyleKit.fontsKey]?[styleKey] as? String,
-         let font =  UIFont(name: fontName, size: CGFloat(size)) {
-        
-        result = font
       }
     }
     
